269106d3d679d7f016c0bbb2d592d6be4d48849f
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2026-09-13 17:00:07 +0330

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,53 @@
+// // import { StrictMode } from "react";
+// // import { createRoot } from "react-dom/client";
+// // import "./index.css";
+// // import { BrowserRouter } from "react-router-dom";
+// // import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// // import { AuthProvider } from "./context/AuthContext";
+// // import App from "./App";
+
+// // const queryClient = new QueryClient();
+
+// // async function prepare() {
+// //   if (import.meta.env.DEV) {
+// //     const { worker } = await import("./mocks/browser");
+// //     return worker.start({ onUnhandledRequest: "bypass" });
+// //   }
+// // }
+
+// // prepare().then(() => {
+// //   createRoot(document.getElementById("root")!).render(
+// //     <StrictMode>
+// //       <QueryClientProvider client={queryClient}>
+// //         <BrowserRouter>
+// //           <AuthProvider>
+// //             <App />
+// //           </AuthProvider>
+// //         </BrowserRouter>
+// //       </QueryClientProvider>
+// //     </StrictMode>,
+// //   );
+// // });
 // import { StrictMode } from "react";
 // import { createRoot } from "react-dom/client";
 // import "./index.css";
+// import "./i18n";
 // import { BrowserRouter } from "react-router-dom";
 // import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import { AuthProvider } from "./context/AuthContext";
 // import App from "./App";
 
-// const queryClient = new QueryClient();
+// // const queryClient = new QueryClient();
+// // توی QueryClient default تنظیم کن (main.tsx یا App.tsx)
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       staleTime: 50_000, // 1 دقیقه
+//       gcTime: 5 * 60_000, // 5 دقیقه در memory نگه داره
+//       retry: 1,
+//     },
+//   },
+// });
 
 // async function prepare() {
 //   if (import.meta.env.DEV) {
@@ -37,23 +78,21 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./context/AuthContext";
 import App from "./App";
 
-// const queryClient = new QueryClient();
-// توی QueryClient default تنظیم کن (main.tsx یا App.tsx)
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 50_000, // 1 دقیقه
-      gcTime: 5 * 60_000, // 5 دقیقه در memory نگه داره
+      staleTime: 50_000,
+      gcTime: 5 * 60_000,
       retry: 1,
     },
   },
 });
 
 async function prepare() {
-  if (import.meta.env.DEV) {
-    const { worker } = await import("./mocks/browser");
-    return worker.start({ onUnhandledRequest: "bypass" });
-  }
+  const { worker } = await import("./mocks/browser");
+  return worker.start({
+    onUnhandledRequest: "bypass",
+  });
 }
 
 prepare().then(() => {
@@ -66,6 +105,6 @@ prepare().then(() => {
           </AuthProvider>
         </BrowserRouter>
       </QueryClientProvider>
-    </StrictMode>,
+    </StrictMode>
   );
 });
